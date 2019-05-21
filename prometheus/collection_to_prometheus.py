4c6678ac01852563ef47da9b0ca672e2e7d96edd
SpiderZhong <root@ihack.cc>
2019-05-21 15:33:18 +0800

    Update collection_to_prometheus.py

--- prometheus/collection_to_prometheus.py
+++ prometheus/collection_to_prometheus.py
@@ -84,10 +84,9 @@ def getMonitorData(rrd_file):
 
 
 if __name__ == '__main__':
-    data_dir = '/opt/smokeping/htdocs/data/'
     ISP_list = ['TELCOM' , 'CMCC' , 'UNICOM' , 'TENCENT']
     for ISP in ISP_list:
-        rrd_data_dir = os.path.join(data_dir, ISP)
+        rrd_data_dir = os.path.join(paras["data_dir"], ISP)
         for filename in os.listdir(rrd_data_dir):
             (instance , postfix) = os.path.splitext(filename)
             if postfix == '.rrd' :
